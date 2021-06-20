f423551bf1e6af995505695477612911987d65c1
chick_0 <chick_0@ch1ck.xyz>
2021-06-21 00:49:16 +0900

    게임 승/패 감지 & Hit/Stand 처리

--- app/template_filter.py
+++ app/template_filter.py
@@ -27,3 +27,21 @@ def get_number(card: str) -> str:
 def calc_total(hand: list) -> str:
     from .card import calc_total
     return str(calc_total(hand=hand))
+
+
+def get_alert_class(win: bool) -> str:
+    if win is True:
+        return "alert-success"
+    elif win is False:
+        return "alert-danger"
+    else:
+        return "alert-secondary"
+
+
+def get_win_string(win: bool) -> str:
+    if win is True:
+        return "승리!"
+    elif win is False:
+        return "패배..."
+    else:
+        return "무승부."
--- app/views/game.py
+++ app/views/game.py
@@ -9,6 +9,7 @@ from flask import redirect
 from flask import render_template
 
 from app.nickname import get_nickname
+from app.card import calc_total
 
 bp = Blueprint(
     name="game",
@@ -59,9 +60,36 @@ def table(session_id):
 
     do = request.args.get("do")
     if do == "hit":
+        if calc_total(hand=game['you']['hand']) < 16:
+            your_card = game['card'].pop()
+            game['you']['hand'].append(your_card)
+            del your_card
+
+        my_card = game['card'].pop()
+        game['me']['hand'].append(my_card)
+        del my_card
+
         "hit!"
+        session[session_id] = game
+
+        if calc_total(hand=game['me']['hand']) > 21:
+            return redirect(url_for("game.table", session_id=session_id, do="stand"))
     elif do == "stand":
+        while True:
+            if calc_total(hand=game['you']['hand']) < 16:
+                your_card = game['card'].pop()
+                game['you']['hand'].append(your_card)
+                del your_card
+            else:
+                break
+
+        # game is end!
+        game['playing'] = False
+
         "stand!"
+        session[session_id] = game
+
+        return redirect(url_for("game.end", session_id=session_id))
 
     return render_template(
         "game/table.html",
@@ -71,4 +99,43 @@ def table(session_id):
 
 @bp.route("/<string:session_id>/end")
 def end(session_id: str):
-    return f"game.end ? session_id={session_id}"
+    game = session.get(session_id, None)
+    if game is None:
+        return redirect(url_for("game.new_game"))
+
+    if game['playing'] is True:
+        return redirect(url_for("game.table", session_id=session_id))
+
+    you = calc_total(hand=game['you']['hand'])
+    me = calc_total(hand=game['me']['hand'])
+
+    if me == 21:
+        win = True
+        reason = "축하드립니다! 21을 만들었습니다!"
+    elif you == 21:
+        win = False
+        reason = f"이런! <b>{game['you']['name']}</b>(이)가 21을 만들었습니다!"
+    elif me > 21:
+        win = False
+        reason = "이런! 당신의 숫자 합이 21보다 크네요..."
+    elif you > 21:
+        win = True
+        reason = f"우와! <b>{game['you']['name']}</b>(이)의 숫자 합이 21보다 크네요"
+    elif me < you:
+        win = False
+        reason = f"이런! 당신의 숫자합이 <b>{game['you']['name']}</b>(이)의 숫자 합 보다 작네요..."
+    elif me > you:
+        win = True
+        reason = f"축하드립니다! <b>{game['you']['name']}</b>(이) 보다 큰 숫자를 만들었습니다!"
+    else:
+        win = None
+        reason = "무승부..."
+
+    del session[session_id]
+    return render_template(
+        "game/end.html",
+        game=game,
+
+        win=win,
+        reason=reason
+    )
