292689e010884224867104c4b0be2470c0a4a4e2
chick_0 <chick_0@ch1ck.xyz>
2021-06-21 02:39:02 +0900

    게임 수정

--- app/views/game.py
+++ app/views/game.py
@@ -25,7 +25,7 @@ def new_game():
         "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DX", "DJ", "DQ", "DK",
         "HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HX", "HJ", "HQ", "HK",
         "SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "SX", "SJ", "SQ", "SK",
-    ]
+    ] * 2
 
     shuffle(card)
 
