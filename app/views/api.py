56c1bb7983b690f992f0c1c1db44773bccd71765
chick_0 <chick_0@ch1ck.xyz>
2021-06-24 02:50:08 +0900

    Stand API 개선

--- app/views/api.py
+++ app/views/api.py
@@ -83,8 +83,10 @@ def hit(session_id):
 @bp.route("/stand/<string:session_id>")
 def stand(session_id):
     game = session.get(session_id, None)
-    if game is None or game['playing'] is False:
-        return redirect(url_for("lobby.index", game="not-found"))
+    if game is None:
+        return jsonify({
+            "game": "not found"
+        })
 
     while True:
         if hit_or_stand(total=calc_total(hand=game['you']['hand'])) and game['you']['stand'] is False:
@@ -94,10 +96,64 @@ def stand(session_id):
         else:
             break
 
-    # game is end!
-    game['playing'] = False
-
     "stand!"
-    session[session_id] = game
 
-    return redirect(url_for("game.end", session_id=session_id))
+    # game is end!
+    del session[session_id]
+
+    # # # # # # # # # # # # # # # # # # # # # # #
+
+    you = calc_total(hand=game['you']['hand'])
+    me = calc_total(hand=game['me']['hand'])
+
+    if me == 21:
+        win = True
+        reason = "축하드립니다! 21을 만들었습니다!"
+    elif you == 21:
+        win = False
+        reason = f"이런! <b>{game['you']['name']}</b>(이)가 21을 만들었습니다!"
+    elif me > 21:
+        win = False
+        reason = "이런! 당신의 숫자 합이 21보다 크네요..."
+    elif you > 21:
+        win = True
+        reason = f"우와! <b>{game['you']['name']}</b>(이)의 숫자 합이 21보다 크네요"
+    elif me < you:
+        win = False
+        reason = f"이런! 당신의 숫자합이 <b>{game['you']['name']}</b>(이)의 숫자 합 보다 작네요..."
+    elif me > you:
+        win = True
+        reason = f"축하드립니다! <b>{game['you']['name']}</b>(이) 보다 큰 숫자를 만들었습니다!"
+    else:
+        win = None
+        reason = ""
+
+    head = {
+        True: "승리!",
+        False: "패배..."
+    }.get(win, "무승부")
+
+    alert_class = {
+        True: "alert-success",
+        False: "alert-danger"
+    }.get(win, "alert-secondary")
+
+    print(game['me']['hand'])
+
+    return jsonify({
+        "game": "end",
+        "alert": {
+            "head": head,
+            "body": reason,
+            "class": f"alert {alert_class}"
+        },
+        "you": {
+            "total": you,
+            "hand": [
+                {
+                    "alt": get_display_card_name(card=card_),
+                    "src": f"/static/card_img/{card_}.png",
+                } for card_ in game['you']['hand']
+            ]
+        }
+    })
