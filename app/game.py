1e93629c92d3bec0ee5f5cf22fe92ba6453d1b21
chick_0 <chick_0@ch1ck.xyz>
2021-06-24 09:39:10 +0900

    게임 세션 리셋 기능 추가

---
+++ app/game.py
@@ -0,0 +1,35 @@
+from random import shuffle
+
+from .nickname import get_nickname
+
+
+def get_card_deck() -> list:
+    return [
+        "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CX", "CJ", "CQ", "CK",
+        "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DX", "DJ", "DQ", "DK",
+        "HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HX", "HJ", "HQ", "HK",
+        "SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "SX", "SJ", "SQ", "SK",
+    ] * 2
+
+
+def get_dummy_session() -> dict:
+    card = get_card_deck()
+    shuffle(card)
+
+    you, me = [], []
+
+    you.append(card.pop())
+    me.append(card.pop())
+
+    return {
+        "card": card,
+        "you": {
+            "name": get_nickname(),
+            "hand": you,
+            "stand": False
+        },
+        "me": {
+            "name": get_nickname(),
+            "hand": me,
+        }
+    }
--- app/views/game.py
+++ app/views/game.py
@@ -1,5 +1,4 @@
 from uuid import uuid4
-from random import shuffle
 
 from flask import Blueprint
 from flask import session
@@ -7,7 +6,7 @@ from flask import url_for
 from flask import redirect
 from flask import render_template
 
-from app.nickname import get_nickname
+from app.game import get_dummy_session
 
 bp = Blueprint(
     name="game",
@@ -18,31 +17,8 @@ bp = Blueprint(
 
 @bp.route("/new")
 def new_game():
-    card = [
-        "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CX", "CJ", "CQ", "CK",
-        "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DX", "DJ", "DQ", "DK",
-        "HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HX", "HJ", "HQ", "HK",
-        "SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "SX", "SJ", "SQ", "SK",
-    ] * 2
-
-    shuffle(card)
-
-    computer = card.pop()
-    me = card.pop()
-
     session_id = str(uuid4())
-    session[session_id] = {
-        "card": card,
-        "you": {
-            "name": get_nickname(),
-            "hand": [computer],
-            "stand": False
-        },
-        "me": {
-            "name": get_nickname(),
-            "hand": [me],
-        }
-    }
+    session[session_id] = get_dummy_session()
 
     return redirect(url_for("game.table", session_id=session_id))
 
