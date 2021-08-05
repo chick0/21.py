3be8cf2afadd02b9c24e6b1fefbab2e899f3394a
chick_0 <chick_0@ch1ck.xyz>
2021-08-05 16:00:01 +0900

    . 추가

--- app/winner.py
+++ app/winner.py
@@ -12,7 +12,7 @@ def get_winner(you: int, me: int, name: dict) -> (bool, str):
     elif me == you:
         # 플레이어와 컴퓨터의 숫자 합이 같은지 확인
         win = None
-        reason = f"모든 플레이어의 숫자 합이 {me}로 동일합니다"
+        reason = f"모든 플레이어의 숫자 합이 {me}로 동일합니다."
 
     elif me > 21 or you > 21:
         # 플레이어 또는 컴퓨터의 숫자 합이 21을 넘었는지 확인
@@ -27,28 +27,28 @@ def get_winner(you: int, me: int, name: dict) -> (bool, str):
                 # 플레이어의 숫자 합이 더 작은 경우
                 if me < you:
                     win = True
-                    reason = f"당신의 숫자 합이 21보다 크지만, <b>{name['you']}</b>보다 숫자 합이 21에 가깝습니다"
+                    reason = f"당신의 숫자 합이 21보다 크지만, <b>{name['you']}</b>보다 숫자 합이 21에 가깝습니다."
 
                 # 플레이어의 숫자 합이 더 큰 경우
                 else:
                     win = False
-                    reason = f"모든 플레이어의 숫자 합이 21보다 크지만, <b>{name['you']}</b>(이)의 숫자 합이 21에 가깝습니다"
+                    reason = f"모든 플레이어의 숫자 합이 21보다 크지만, <b>{name['you']}</b>(이)의 숫자 합이 21에 가깝습니다."
             else:
                 # 플레이어의 숫자 합은 21을 넘었고,
                 #   컴퓨터의 숫자 합은 21을 넘지 않았음
                 win = False
-                reason = "당신의 숫자 합이 21보다 큽니다"
+                reason = "당신의 숫자 합이 21보다 큽니다."
 
         else:
             # 플레이어의 숫자 합은 21을 넘지 않았고,
             #   컴퓨터의 숫자 합은 21을 넘은 경우
             win = True
-            reason = f"축하드립니다! <b>{name['you']}</b>(이)의 숫자 합이 21보다 큽니다"
+            reason = f"축하드립니다! <b>{name['you']}</b>(이)의 숫자 합이 21보다 큽니다."
 
     elif me < you:
         # 플레이어의 숫자 합이 컴퓨터 보다 작은 경우
         win = False
-        reason = f"당신의 숫자합이 <b>{name['you']}</b>(이)의 숫자 합 보다 작습니다"
+        reason = f"당신의 숫자합이 <b>{name['you']}</b>(이)의 숫자 합 보다 작습니다."
 
     elif me > you:
         # 플레이어의 숫자 합이 컴퓨터 보다 큰 경우
