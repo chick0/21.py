c18a33f07352fdf7a58b5a9a1fa1810a22e6c7a0
chick_0 <chick_0@ch1ck.xyz>
2022-06-15 01:03:23 +0900

    create keyboard event handler

--- app/static/21.js
+++ app/static/21.js
@@ -8,25 +8,29 @@ function call_stand(){
     axios({
         method: "GET",
         url: api.stand
-    }).then(function(resp){
+    }).then((resp) => {
         if(resp.data.game == "end"){
-            document.getElementById("your_hand").innerHTML="";
-            document.getElementById("winning_rate_text").innerText=resp.data.count.winning_rate+"%";
-            resp.data.you.hand.forEach(function(card){
-                var you = document.createElement("img");
-                you.setAttribute("src", cards[card.src]);
-                you.setAttribute("alt", card.alt);
+            document.getElementById("your_hand").innerHTML = "";
+            resp.data.you.hand.forEach((card) => {
+                let img = document.createElement("img");
+                img.setAttribute("src", cards[card.src]);
+                img.setAttribute("alt", card.alt);
+
                 document.getElementById("your_hand").appendChild(document.createTextNode(" "));
-                document.getElementById("your_hand").appendChild(you);
+                document.getElementById("your_hand").appendChild(img);
             });
-            document.getElementById("your_total").innerText=resp.data.you.total;
+
+            document.getElementById("winning_rate_text").innerText=resp.data.count.winning_rate+"%";
+
+            document.getElementById("your_total").innerText = resp.data.you.total;
+
             document.getElementById("statusHead").setAttribute("style", `color:${resp.data.alert.color}`);
-            document.getElementById("statusHead").innerText=resp.data.alert.head;
-            document.getElementById("statusBody").innerHTML=resp.data.alert.body;
+            document.getElementById("statusHead").innerText = resp.data.alert.head;
+            document.getElementById("statusBody").innerHTML = resp.data.alert.body;
 
             document.getElementById("game-total").innerText = resp.data.count.total;
             document.getElementById("game-win").innerText = resp.data.count.win;
-            document.getElementById("game-rate").innerText=resp.data.count.winning_rate + " %";
+            document.getElementById("game-rate").innerText = resp.data.count.winning_rate + " %";
 
             next_name = resp.data.name.you;
 
@@ -38,32 +42,36 @@ function call_stand(){
         else if(resp.data.game == "not found"){
             location.reload();
         }
-    }).catch(function(err){
+    }).catch((err) => {
         console.log(err);
         window.alert("오류가 발생했습니다. 버튼을 다시 눌러주십시오.");
     });
 }
 
-hit.addEventListener("click", function(){
+hit.addEventListener("click", () => {
     axios({
         method: "GET",
         url: api.hit
-    }).then(function(resp){
+    }).then((resp) => {
         if(resp.data.game == "ok"){
             if(resp.data.you){
-                var you = document.createElement("img");
-                you.setAttribute("src", cards['back']);
-                you.setAttribute("alt", "Hidden Card");
+                let img = document.createElement("img");
+                img.setAttribute("src", cards['back']);
+                img.setAttribute("alt", "Hidden Card");
+
                 document.getElementById("your_hand").appendChild(document.createTextNode(" "));
-                document.getElementById("your_hand").appendChild(you);
+                document.getElementById("your_hand").appendChild(img);
+
+                // cant check your total
             }
 
-            var me = document.createElement("img");
+            let me = document.createElement("img");
             me.setAttribute("src", cards[resp.data.me.src]);
             me.setAttribute("alt", resp.data.me.alt);
             document.getElementById("my_hand").appendChild(document.createTextNode(" "));
             document.getElementById("my_hand").appendChild(me);
-            document.getElementById("my_total").innerText=resp.data.me.total;
+
+            document.getElementById("my_total").innerText = resp.data.me.total;
         }
         else if(resp.data.game == "not found"){
             location.reload();
@@ -72,15 +80,18 @@ hit.addEventListener("click", function(){
             call_stand();
         }
         else if(resp.data.game == "bust with new card"){
-            var me = document.createElement("img");
+            let me = document.createElement("img");
             me.setAttribute("src", cards[resp.data.me.src]);
             me.setAttribute("alt", resp.data.me.alt);
+
             document.getElementById("my_hand").appendChild(document.createTextNode(" "));
             document.getElementById("my_hand").appendChild(me);
-            document.getElementById("my_total").innerText=resp.data.me.total;
+
+            document.getElementById("my_total").innerText = resp.data.me.total;
+
             call_stand();
         }
-    }).catch(function(err){
+    }).catch((err) => {
         console.log(err);
         window.alert("오류가 발생했습니다. 버튼을 다시 눌러주십시오.");
     });
@@ -91,32 +102,48 @@ reset.addEventListener("click", function(){
     axios({
         method: "GET",
         url: api.status,
-    }).then(function(resp){
-        document.getElementById("your_total").innerText=resp.data.you.total;
-        document.getElementById("my_total").innerText=resp.data.me.total;
-        document.getElementById("your_hand").innerHTML="";
-        document.getElementById("my_hand").innerHTML="";
-        resp.data.you.hand.forEach(function(card){
-            var you = document.createElement("img");
-            you.setAttribute("src", cards[card.src]);
-            you.setAttribute("alt", card.alt);
+    }).then((resp) => {
+        document.getElementById("your_total").innerText = resp.data.you.total;
+        document.getElementById("your_hand").innerHTML = "";
+        resp.data.you.hand.forEach((card) => {
+            let img = document.createElement("img");
+            img.setAttribute("src", cards[card.src]);
+            img.setAttribute("alt", card.alt);
+
             document.getElementById("your_hand").appendChild(document.createTextNode(" "));
-            document.getElementById("your_hand").appendChild(you);
+            document.getElementById("your_hand").appendChild(img);
         });
-        resp.data.me.hand.forEach(function(card){
-            var me = document.createElement("img");
-            me.setAttribute("src", cards[card.src]);
-            me.setAttribute("alt", card.alt);
+
+        document.getElementById("my_total").innerText = resp.data.me.total;
+        document.getElementById("my_hand").innerHTML = "";
+        resp.data.me.hand.forEach((card) => {
+            let img = document.createElement("img");
+            img.setAttribute("src", cards[card.src]);
+            img.setAttribute("alt", card.alt);
+
             document.getElementById("my_hand").appendChild(document.createTextNode(" "));
-            document.getElementById("my_hand").appendChild(me);
+            document.getElementById("my_hand").appendChild(img);
         });
 
         document.body.classList.remove("is-clipped");
         document.getElementById("showGameStatus").classList.remove("is-active");
 
-        document.getElementById("you-name").innerText = next_name;
-    }).catch(function(err){
+        if(next_name.length != 0)
+            document.getElementById("you-name").innerText = next_name;
+    }).catch((err) => {
         console.log(err);
         location.reload();
     });
+});
+
+document.addEventListener('keydown', (key) => {
+    key = key.key.toLowerCase();
+
+    if(key == 'h'){
+        hit.click();
+    } else if(key == 's') {
+        stand.click();
+    } else if(key == 'r') {
+        reset.click();
+    }
 });
